3899cd2f3d0ba659b6aa186305f5dfa8968c83a6
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2023-10-08 10:26:08 -0400

    Custom tab

---
+++ AnTaIcAnSFSym/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  AnTaIcAnSFSym
+//
+//  Created by Kartikeya Saxena Jain on 10/8/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
