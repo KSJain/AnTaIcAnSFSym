ed55cf8e46298c3d4481f6695c2522478042c26d
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2023-10-08 10:28:03 -0400

    weird

---
+++ AnTaIcAnSFSym/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  AnTaIcAnSFSym
+//
+//  Created by Kartikeya Saxena Jain on 10/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
--- AnTaIcAnSFSym/View/HomeView.swift
+++ AnTaIcAnSFSym/View/HomeView.swift
@@ -8,11 +8,102 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State private var activeTab: Tab = .photos
+    @State private var allTabs: [AnimatedTab] = Tab.allCases.compactMap { tab -> AnimatedTab? in
+        return .init(tab: tab)
     }
+    
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            TabView(selection: $activeTab) {
+                
+                NavigationStack {
+                    VStack {
+                        
+                    }
+                    .navigationTitle(Tab.photos.title)
+                }
+                .setupTab(.photos)
+                
+                NavigationStack {
+                    VStack {
+                        
+                    }
+                    .navigationTitle(Tab.chat.title)
+                }
+                .setupTab(.chat)
+                
+                NavigationStack {
+                    VStack {
+                        
+                    }
+                    .navigationTitle(Tab.apps.title)
+                }
+                .setupTab(.apps)
+                
+                NavigationStack {
+                    VStack {
+                        
+                    }
+                    .navigationTitle(Tab.notifications.title)
+                }
+                .setupTab(.notifications)
+                
+                NavigationStack {
+                    VStack {
+                        
+                    }
+                    .navigationTitle(Tab.profile.title)
+                }
+                .setupTab(.profile)
+                
+            }
+            
+            CustomTabBar()
+        }
+    }
+    
+    @ViewBuilder
+    func CustomTabBar() -> some View {
+        HStack(spacing: 0) {
+            ForEach($allTabs) { $animatedTab in
+                let tab = animatedTab.tab
+                
+                VStack(spacing: 4) {
+                    Image(systemName: tab.rawValue)
+                        .font(.title2)
+                    
+                    Text(tab.title)
+                        .font(.caption2)
+                        .textScale(.secondary)
+                }
+                .frame(maxWidth: .infinity)
+                .foregroundStyle(activeTab == tab ? Color.primary : Color.gray.opacity(0.8))
+                .padding(.top, 15)
+                .padding(.bottom, 10)
+                .contentShape(.rect)
+                .onTapGesture {
+                    activeTab = tab
+                }
+            }
+        }
+        .background(.bar)
+    }
+    
 }
 
 #Preview {
-    HomeView()
+    ContentView()
+}
+
+
+extension View {
+    @ViewBuilder
+    func setupTab(_ tab: Tab) -> some View {
+        self
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .tag(tab)
+            .toolbar(.hidden, for: .tabBar)
+    }
 }
