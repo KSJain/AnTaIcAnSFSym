0c31f378b30853d1b2ff83635c52d4a36ff825b5
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2023-10-08 09:33:58 -0400

    Initial Commit

---
+++ AnTaIcAnSFSym/AnTaIcAnSFSymApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnTaIcAnSFSymApp.swift
+//  AnTaIcAnSFSym
+//
+//  Created by Kartikeya Saxena Jain on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AnTaIcAnSFSymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
