c234c9a44a7e0b7cffdfa6f7e125be0349bd5a42
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2023-10-08 09:42:15 -0400

    Model - Tab

---
+++ AnTaIcAnSFSym/Model/Tab.swift
@@ -0,0 +1,38 @@
+//
+//  Tab.swift
+//  AnTaIcAnSFSym
+//
+//  Created by Kartikeya Saxena Jain on 10/8/23.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable {
+    case photos = "photo.stack"
+    case chat = "bubble.left.and.text.bubble.right"
+    case apps = "square.3.layers.3d"
+    case notifications = "bell.and.waves.left.and.right"
+    case profile = "person.2.crop.square.stack.fill"
+    //.bounce.down.byLayer
+    var title: String {
+        switch self {
+        case .photos:
+            return "Photos"
+        case .chat:
+            return "Chat"
+        case .apps:
+            return "Apps"
+        case .notifications:
+            return "Notifications"
+        case .profile:
+            return "Profile"
+        }
+    }
+}
+
+
+struct AnimatedTab: Identifiable {
+    var id: UUID = .init()
+    var tab: Tab
+    var isAnimated: Bool?
+}
